07945a7060982944ff5a522458d7b94d70434db5
525gtarantola <525gtarantola@frhsd.com>
2024-01-24 11:47:30 -0500

    Pics added

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 function show1496(){
-    document.getElementById("date").innerText = "May 3rd 1496";
+    document.getElementById("date").innerText = "May 3rd, 1496";
     document.getElementById("picAboutDay").src = "https://picsum.photos/200/300";
     
 }
 
 function show1802(){
-    document.getElementById("date").innerText = "May 3rd 1496";
+    document.getElementById("date").innerText = "May 3rd, 1802";
+    document.getElementById("picAboutDay").src = "https://picsum.photos/200/300";
+    
+}
+
+function show1947(){
+    document.getElementById("date").innerText = "May 3rd, 1947";
+    document.getElementById("picAboutDay").src = "https://picsum.photos/200/300";
+    
+}
+
+function show1979(){
+    document.getElementById("date").innerText = "May 3rd, 1979";
     document.getElementById("picAboutDay").src = "https://picsum.photos/200/300";
     
 }
