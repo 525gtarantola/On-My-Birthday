353848b14eff4d88792c52cc5d4b655d0cf0f5eb
525gtarantola <525gtarantola@frhsd.com>
2024-01-24 11:35:53 -0500

    JS

---
+++ script.js
@@ -0,0 +1,11 @@
+function show1496(){
+    document.getElementById("date").innerText = "May 3rd 1496";
+    document.getElementById("picAboutDay").src = "https://picsum.photos/200/300";
+    
+}
+
+function show1802(){
+    document.getElementById("date").innerText = "May 3rd 1496";
+    document.getElementById("picAboutDay").src = "https://picsum.photos/200/300";
+    
+}
